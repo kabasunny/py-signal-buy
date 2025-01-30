e0b100698a69037571037dc642b870cfb5518acd
kabasunny <dxbqd553@yahoo.co.jp>
2025-01-30 18:17:29 +0900

    モデルのマップに正解ラベルのパターンを追加した

--- result/ResultSavingStage.py
+++ result/ResultSavingStage.py
@@ -29,20 +29,16 @@ class ResultSavingStage:
         self,
         symbols: List[str],
         subdir: str,
-    ):  # リストを受けるため他のパイプラインと異なる
+    ):
         responses = []
-        # print(symbols)
         for _, row in symbols.iterrows():
             symbol = row["symbol"]  # symbol 列の値を取得
-            # raw_data_manager からデータを読み込む
             raw_data_df = self.raw_data_manager.load_data(symbol)
 
-            # データフレームが空でないことを確認
             if raw_data_df.empty:
                 print(f" {symbol} をスキップします")
                 continue
 
-            # split_date の翌日以降のデータに絞り込む
             raw_data_df = raw_data_df[raw_data_df["date"] > self.split_date]
 
             daily_data_list = [
@@ -57,22 +53,24 @@ class ResultSavingStage:
                 for _, row in raw_data_df.iterrows()
             ]
 
-            # predictions_data_manager からデータを読み込む
             predictions_df = self.bach_pred_data_manager.load_data_from_subdir(
                 subdir, symbol
             )
 
-            # データフレームが空でないことを確認
             if predictions_df.empty:
                 print(f"データが見つからないため {symbol} をスキップします。")
                 continue
 
-            # split_date の翌日以降のデータに絞り込む
             predictions_df = predictions_df[predictions_df["date"] > self.split_date]
 
             signal_dates = predictions_df[predictions_df["label"] == 1]["date"].tolist()
 
-            model_predictions = {}
+            # モデルのマップに正解ラベルのパターンを追加した
+            model_predictions = {
+                "correct_label": ModelPredictions(prediction_dates=signal_dates)
+            }
+
+            # モデルの結果のマップ
             for model in self.model_types:
                 prediction_dates = predictions_df[predictions_df[model] == 1][
                     "date"
@@ -90,10 +88,8 @@ class ResultSavingStage:
 
             responses.append(symbol_data)  # MLSymbolData を直接追加
 
-        # MLSymbolData のリストを含む結合レスポンスを作成
         combined_response = MLStockResponse(symbol_data=responses)
 
-        # 保存処理を実行
         self.proto_saver_loader.save_proto_response_to_file(
             combined_response,
             f'proto_{subdir.replace("/", "-")}.bin',
